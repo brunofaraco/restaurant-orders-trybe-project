83234d7865084e3ee963e00da55727577cc7c247
Bruno <brunocfaraco@hotmail.com.br>
2023-05-17 15:01:37 -0300

    feat: req 3 conclusion

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,24 @@
+import pandas as p
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+
 # Req 3
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        self.dishes_path = p.read_csv(source_path).itertuples(index=False)
+        self.dishes_map()
+
+    def dishes_map(self):
+        for name, price, ingredient, amount in self.dishes_path:
+            dish_instance = Dish(name, float(price))
+
+            if dish_instance not in self.dishes:
+                dish_instance.add_ingredient_dependency(Ingredient(ingredient), int(amount))
+                self.dishes.add(dish_instance)
+            else:
+                for dish in self.dishes:
+                    if dish == dish_instance:
+                        dish.add_ingredient_dependency(
+                            Ingredient(ingredient), int(amount)
+                        )
